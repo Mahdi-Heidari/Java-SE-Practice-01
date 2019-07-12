e92ae43ca1b22c9cc854006050d8b7d020fc2c1b
Mahdi Heidari <mheidarim2001@gmail.com>
2019-07-12 15:00:54 +0430

    Update JavaSEPractice01.java

--- JavaSEPractice01.java
+++ JavaSEPractice01.java
@@ -300,4 +300,21 @@ public class JavaSEPractice01 {
         return perimeter;
         //        return x * y;
     }
+    
+//        ****************************************************************
+
+    public static int maxDigit(int a) {
+
+        int max = 0;
+        a = Math.abs(a);
+
+        while (a != 0) {
+
+            if (max < a % 10) {
+                max = a % 10;
+            }
+            a /= 10;
+        }
+        return max;
+    }
 }
